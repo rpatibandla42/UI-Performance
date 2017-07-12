02a0abe68d99eb13c77a5693159e792ccb0d3a64
rpatibandla42 <rpatibandla242@gmail.com>
2017-07-11 21:13:11 -0700

    changing property value's in a object

---
+++ UI-Performance/object.js
@@ -0,0 +1,28 @@
+var x={
+ firstName: 'Ramya',
+ age: 25,
+ gender: 'Female',
+ isPresent: false,
+ isSuspended: null,
+  y:{
+  lastName: 'Patibandla',
+    z:{
+    age: 20
+
+    }
+
+  }
+
+};
+
+x.firstName='Venu',
+x.y.lastName='Takkelapti',
+x.y.z.age=50
+
+var a={
+
+   name:'Lakshmi',
+   age:30
+}
+console.log(x);
+console.log(a);
---
+++ UI-Performance/practice.js
@@ -0,0 +1,12 @@
+var a=30;
+var b=43.33;
+
+console.log(a);
+
+
+
+
+
+
+var a=10.33;
+console.log(a);

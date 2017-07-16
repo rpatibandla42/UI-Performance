9527abfee2c4be27cf7d0ad7fcab57dfc556a5cb
rpatibandla42 <rpatibandla242@gmail.com>
2017-07-15 23:23:00 -0700

    functionstypes

---
+++ UI-Performance/Function.js
@@ -0,0 +1,23 @@
+function addition(){
+ var x = 1;
+ var y = x + 2;
+
+ return y;
+}
+
+//console.log(addition());
+
+var test=addition();
+console.log(test);
+//Named functions we can use it for any where
+//anonymous functions
+
+var a=function(){
+var x=1;
+var y=x+1;
+
+return y;
+//no need to write return statement in anonymous functions
+
+}
+console.log(a);

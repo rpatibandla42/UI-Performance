fca9d880e91f8e6052c7ad25bd767936f5594aa8
rpatibandla42 <rpatibandla242@gmail.com>
2017-07-15 23:54:38 -0700

    difference between var output and function output

--- UI-Performance/Function.js
+++ UI-Performance/Function.js
@@ -5,19 +5,6 @@ function addition(){
  return y;
 }
 
-//console.log(addition());
-
-var test=addition();
-console.log(test);
-//Named functions we can use it for any where
-//anonymous functions
-
-var a=function(){
-var x=1;
-var y=x+1;
-
-return y;
-//no need to write return statement in anonymous functions
-
-}
-console.log(a);
+console.log(addition());
+ var addition=3;
+ console.log(addition);

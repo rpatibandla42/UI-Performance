4fb2548e652b3bff2fc267196eed0563e4381bef
rpatibandla42 <rpatibandla242@gmail.com>
2017-07-22 21:51:47 -0700

    by using class converting function o class

--- UI-Performance/class.js
+++ UI-Performance/class.js
@@ -1,16 +1,19 @@
 (function(){
+//This class is now an object that can be reused any number of times
+function x(){
 
-var x={
-  firstName:'Ramya',
-  lastName:'Patibandla'
-};
-console.log(x);
-var y=x;
-
-y.firstName='Siva',
-y.lastName='Varagani'
+this.firstName='Ramya',
+this.lastName='Patibandla'
+}
+console.log(x());
+//creating a new instance of x object(class x)
+var y=new x();
+y.firstName='Narendra',
+y.lastName='Chowdary'
 console.log(y);
 
-
-
+var z=new x();
+z.firstName='Tirumula',
+z.lastName='Chowdar'
+console.log(z);
 })();

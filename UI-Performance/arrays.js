ba185663165e6f9b71be905c9a32c2046ef514c9
rpatibandla42 <rpatibandla242@gmail.com>
2017-07-29 17:22:24 -0700

    Examples on Splice

--- UI-Performance/arrays.js
+++ UI-Performance/arrays.js
@@ -8,4 +8,9 @@ var d = ['Tom', 'Paul', 'Leif', 'Nandigam'];
 d.shift();
 console.log(d);
 
+var names = ['Venu', 'Tarun', 'srikanth', 'Gopi', 'Timmu'];
+//names.splice(2, 1, 'Chowdary');//by using this we can replace chowdary name at srikanth then Srikanth automatically removes it
+names.splice(1, 2);//here removes both tarun and srikanth
+console.log(names);
+
 }) ();

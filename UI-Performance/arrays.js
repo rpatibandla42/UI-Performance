356c5f5a956135ec91f568e1f864756dc8cf5002
rpatibandla42 <rpatibandla242@gmail.com>
2017-07-29 17:07:14 -0700

    Examples of unshift and shift

--- UI-Performance/arrays.js
+++ UI-Performance/arrays.js
@@ -1,49 +1,11 @@
 (function(){
 
- var alpha =
-  [{
-    firstName: 'Ramya',
-    lastName: 'Patibandla'
-  }, {
+var c = ['Neha', 'Harsha'];
+c.unshift('Somu');
+console.log(c);
 
-    firstName: 'Venu',
-    lastName: 'Takkellapati'
-  }];
+var d = ['Tom', 'Paul', 'Leif', 'Nandigam'];
+d.shift();
+console.log(d);
 
-  var beta = ['Srikar',33,false,null];
-  var theta = ['Ramya', 'Narendra', 'Gopi', 'Tirumula']
-   //console.log(alpha);
-   //console.log(beta);
-   console.log(theta[1]);
-   //we can't mention more than one index if we are mentioned no.of index it takes last index to display output
-   console.log(alpha[0].firstName);//it will display output Ramya
-   theta[7] = 'Siva';
-   console.log(theta);
-
-   var omega = new Array();
-   omega[0] = 'Ramay',
-   omega[1] = 'Narendra',
-   omega[2] = 'Gopi',
-   omega[3] = 'Tirumula'
-
-   console.log(omega.length);//it showing length is 4
-   var delta = 33;
-   console.log(Array.isArray(delta));//It display false because delta is not a class
-   //by using this we can check variable is array or not.
-   /*
-   omega.concat means omega is object contact is property of object
-   */
-   /* function Array(){
-      Arraty is function to convert to class we are using this keyword actually
-   }
-   here omega cloning the data of Array
-  */
-
- var a = ['Ramya', 'Patibandla'];
- console.log(a.push('Venu'));
-//a.push('Venu'); It means add something to the Array
-
- var b = ['Ramya', 'Narendra', 'Revathi', 'Shabeera'];
-  b.pop();
-  console.log(b);
 }) ();

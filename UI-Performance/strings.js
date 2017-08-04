1b62b036eba2529ff17693e8ed9e6de71bb2d052
rpatibandla42 <rpatibandla242@gmail.com>
2017-08-04 16:14:27 -0700

    this is about character replacement

---
+++ UI-Performance/strings.js
@@ -0,0 +1,11 @@
+//IIFE Construct
+
+(function(){
+
+ var test = 'Ramya';
+ var test1 = test.replace('a','v');
+
+ //var output = test + test1;
+//+-> This performs a string concatination it means merging two strings.
+ console.log(test1);
+})();

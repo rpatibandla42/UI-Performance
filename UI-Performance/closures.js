2707ea1102a7acb9649ae712cf80d13e14082326
rpatibandla42 <rpatibandla242@gmail.com>
2017-08-04 12:35:53 -0700

    example on clousers

--- UI-Performance/closures.js
+++ UI-Performance/closures.js
@@ -1,14 +1,47 @@
-function sum(){
+(function(){
+//class matematics
+//function name must start letter capital.
+function Total(){
+ var tot = this;
 
- var _sum = this;//always create alias for this keyword
- _sum.a = 1;
 
-  _sum.difference = function(){
 
-   var _self = this;
-   _self.a = 5;
-   _sum.a = 6;
-  }
- }
-var result = new sum();
-console.log(result.a);
+
+   function Mathematics(num1, num2){
+    var math = this;
+    math.sum = function(){
+     return(num1 + num2);
+ };
+
+  math.difference = function(){
+      return(num1 - num2);
+  };
+
+  math.multiplication = function(){
+        return(num1 * num2);
+  };
+
+   math.division = function(){
+          return(num1 / num2);
+   };
+}
+
+    var operation = new Mathematics(100, 230);
+
+    tot.addition = operation.sum();
+    tot.subtraction = operation.difference();
+    tot.product = operation.multiplication();
+    tot.division = operation.division();
+}
+    var result = new Total();
+
+    console.log(result);
+
+
+
+
+
+
+
+
+})();

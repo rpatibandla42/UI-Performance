ca83f1b1d2a9badbbc008b288db28fef0404649b
rpatibandla42 <rpatibandla242@gmail.com>
2017-08-04 17:37:13 -0700

    split example

--- UI-Performance/strings.js
+++ UI-Performance/strings.js
@@ -2,10 +2,10 @@
 
 (function(){
 
- var test1 = 'Ramya Patibandla is good girl';
- var test2 = 'Ramya is good girl';
+ var test1 = 'Ramya , Patibandla , has , a';
+ //var test2 = 'Ramya is good girl';
 
    //var test3 = test.match(newTest);
 
- console.log(test1.match(test2));
+ console.log(test1.split(','));
 })();

c8a89ba122063f00a15db96c0d90cd3f84415bbd
rpatibandla42 <rpatibandla242@gmail.com>
2017-08-13 00:30:19 -0700

    example on If else statement

---
+++ UI-Performance/conditions.js
@@ -0,0 +1,17 @@
+(function(){
+
+   var test = [22,33,54,43,23,65,54,44,88,506];
+   var name = '4';
+   var num = 4;
+
+  if(name === num){
+
+   console.log('This is true');
+  }
+
+  else{
+
+   console.log('This is false');
+  }
+
+})();

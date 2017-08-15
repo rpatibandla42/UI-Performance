52b5f69cc0e5e01ead6dc989e7257728a936a83a
rpatibandla42 <rpatibandla242@gmail.com>
2017-08-14 23:19:48 -0700

    onload example

--- UI-Performance/conditions.js
+++ UI-Performance/conditions.js
@@ -1,22 +1,10 @@
-(function(){
-//To access an element H1 we can use a in build object call, named as 'getElementById'
+(function(window){
 
-  //console.log(document.getElementById('Heading'));
-   //var x = getElementById('Heading');
-   console.log(document.getElementById('Heading'));
-   console.log(document.getElementsByClassName('test')[2]);
-   //by using index 3 here we can access only 3rd line
-   console.log(document.getElementsByTagName('p'));
-   //This is for select all p tags
-   console.log(document.getElementsByTagName('p')[1]);
-   //In tags also we can mention index also
+     window.myTest = function(){
 
-   console.log(document.querySelector('.test'));
-   //This is for selecting a class elemnt by using .
-   console.log(document.querySelector('p'));
-   //By using this we can select html elemnt # and also we can select tag also
+       console.log('Hello Ramya');
 
+     }
 
-
-})();
+})(window);
 
